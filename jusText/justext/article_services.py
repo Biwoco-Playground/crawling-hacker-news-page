7f295c4271c29d7e27c46d249709dbe5875238b3
vkhanhqui <vkhanhqui@gmail.com>
2021-06-29 15:22:09 +0700

    get context sensitive class and main content

--- jusText/justext/article_services.py
+++ jusText/justext/article_services.py
@@ -1,5 +1,4 @@
 import stopwords_manager
-import re
 import utils
 
 from lxml.html.clean import Cleaner
@@ -10,7 +9,7 @@ from models import Block
 MAX_LINK_DENSITY = 0.2
 LENGTH_LOW = 10
 LENGTH_HIGH = 30
-STOPWORDS_LOW = 0.30
+STOPWORDS_LOW = 0.20
 STOPWORDS_HIGH = 0.32
 PARAGRAPH_TAGS = [
     'body', 'blockquote', 'caption', 'center', 'col', 'colgroup', 'dd',
@@ -18,6 +17,7 @@ PARAGRAPH_TAGS = [
     'p', 'pre', 'table', 'td', 'textarea', 'tfoot', 'th', 'thead', 'tr',
     'ul', 'li', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
 ]
+MAIN_TAGS = ['h1', 'h2']
 
 
 def preprocess(str_html):     
@@ -54,10 +54,10 @@ def divide_blocks(str_html):
     language = stopwords_manager.map_language(language_code)
     stopwords = stopwords_manager.get_stoplist(language)
     for tag in tree.iter():
-        current_tag = tag.tag
-        if current_tag in PARAGRAPH_TAGS:
+        current_tag = tag.tag        
+        if (current_tag in PARAGRAPH_TAGS):
             text = tag.text
-            if text is not None:
+            if (text is not None):
                 content = text.split(" ")
                 total_words = len(content)
                 count_stopwords = 0
@@ -65,11 +65,6 @@ def divide_blocks(str_html):
                     if sub in stopwords:
                         count_stopwords += 1
                 stopwords_density = count_stopwords/total_words
-
-                block = Block(
-                            text, total_words, 
-                            stopwords_density)
-
                 count_a = 0
                 link_density = 0
                 for child in tag:
@@ -81,11 +76,18 @@ def divide_blocks(str_html):
 
                 if count_a > 0:
                     link_density /= count_a
-
-                block.link_density = link_density
-                block.CLASS = classify_block(
+                type = ""
+                if current_tag in MAIN_TAGS:
+                    type = "good"
+                else:
+                    type = classify_block(
                                             link_density, total_words, 
                                             stopwords_density)
+                
+                block = Block(
+                            text, total_words, 
+                            stopwords_density, link_density,
+                            type)
                 blocks.append(block)
             
     return blocks
@@ -94,25 +96,113 @@ def divide_blocks(str_html):
 def classify_block(
                 link_density, length, 
                 stopwords_density):
-
     if link_density > MAX_LINK_DENSITY:
         return 'bad'
 
-    # short blocks
     if length < LENGTH_LOW:
         if link_density > 0:
             return 'bad'
         else:
             return 'short'
 
-    # medium and long blocks
     if stopwords_density > STOPWORDS_HIGH:
         if length > LENGTH_HIGH:
             return 'good'
         else:
             return 'near-good'
+            
     if stopwords_density > STOPWORDS_LOW:
         return 'near-good'
     else:
         return 'bad'
 
+
+def get_main_content(blocks):
+    main_content = ""
+    index = 0
+    for block in blocks:
+        block_type = block.type
+        # print(block)
+        # print("---------")
+        block.type = get_context_sensitive_class(blocks, block_type, index)
+        # print(block)
+        # print("---------")
+        if block.type == 'good':
+            main_content += block.content + "\n"
+        index += 1
+
+    main_content = utils.normalize_whitespace(main_content)
+    return main_content
+
+
+def get_context_sensitive_class(blocks, block_type, index):
+    if block_type == 'bad':
+        return 'bad'
+    if block_type == 'good':
+        return 'good'
+    if block_type == 'near-good':
+        prev_block_type = get_prev_good_or_bad_block(blocks, index)
+        next_block_type = get_next_good_or_bad_block(blocks, index)
+        if prev_block_type == 'good' or next_block_type == 'good':
+            return 'good'
+        else:
+            return 'bad'
+    if block_type == 'short':
+        prev_block_type = get_prev_good_or_bad_block(blocks, index)
+        next_block_type = get_next_good_or_bad_block(blocks, index)
+        if prev_block_type == 'bad' and next_block_type == 'bad':
+            return 'bad'
+        if prev_block_type == 'good' and next_block_type == 'good':
+            return 'good'
+        if prev_block_type == 'bad' and next_block_type == 'good':
+            if get_prev_non_short_block(blocks, index) == 'near-good':
+                return 'good'
+            else:
+                return 'bad'
+        if next_block_type == 'bad' and prev_block_type == 'good':
+            if get_next_non_short_block(blocks, index) == 'near-good':
+                return 'good'
+            else:
+                return 'bad'
+
+
+def get_prev_good_or_bad_block(blocks, index):
+    i = index
+    while i > 0:
+        i -= 1
+        block_type = blocks[i].type
+        if block_type == 'good':
+            return block_type
+    return 'bad'
+
+
+def get_next_good_or_bad_block(blocks, index):
+    i = index
+    len_blocks = len(blocks)
+    while i < len_blocks - 1:
+        i += 1
+        block_type = blocks[i].type
+        if block_type == 'good':
+            return block_type
+    return 'bad'
+
+
+def get_prev_non_short_block(blocks, index):
+    i = index
+    while i > 0:
+        i -= 1
+        block_type = blocks[i].type
+        if block_type != 'short':
+            return block_type
+    return 'bad'
+
+
+def get_next_non_short_block(blocks, index):
+    i = index
+    len_blocks = len(blocks)
+    while i < len_blocks - 1:
+        i += 1
+        block_type = blocks[i].type
+        if block_type != 'short':
+            return block_type
+    return 'bad'
--- jusText/justext/models.py
+++ jusText/justext/models.py
@@ -4,19 +4,22 @@ class Block:
     content_length = 0
     stopwords_density = 0.0
     link_density = 0.0
-    CLASS = ""
+    type = ""
 
     def __init__(
                 self, content, 
-                content_length, stopwords_density):
+                content_length, stopwords_density,
+                link_density, type):
         self.content = content
         self.content_length = content_length
         self.stopwords_density = stopwords_density
+        self.link_density = link_density
+        self.type = type
 
     def __str__(self):
         return (
-            "content:{0}\ncontent_length:{1}\nstopwords_density:{2}\nlink_density:{3}\nCLASS:{4}"
+            "content:{0}\ncontent_length:{1}\nstopwords_density:{2}\nlink_density:{3}\ntype:{4}"
                                                             .format(
                                                                     self.content, self.content_length,
                                                                     self.stopwords_density, self.link_density,
-                                                                    self.CLASS))
+                                                                    self.type))
